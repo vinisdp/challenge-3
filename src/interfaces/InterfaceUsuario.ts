6a426f6b3faef2947adeb390032a0326178537d7
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-04-05 16:44:30 -0400

    configurações iniciais e interface de usuario

---
+++ src/interfaces/InterfaceUsuario.ts
@@ -0,0 +1,13 @@
+export interface IUser extends Document {
+    firstName: string;
+    lastName: string;
+    birthDate: Date;
+    city: string;
+    country: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    passwordChangedAt: Date;
+    //isCorrectPassword(candidatePassword: string, userPassword: string): Promise<boolean>;
+    //changedPasswordAfter(this: IUser, JWTTimestamp: number): boolean;
+}

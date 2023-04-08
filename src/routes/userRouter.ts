0e71b99676f9aa2926b3789549c094e19ff35acb
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-04-08 08:25:10 -0400

    Update userRouter.ts

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
+import authController from '../controllers/authController';
 
 export const userRouter = Router();
 
-userRouter.post('/signUp');
-userRouter.post('/signIn');
-userRouter.patch('/', userController.update);
-userRouter.delete('/', userController.delete);
+userRouter.post('/signUp', authController.signUp);
+userRouter.post('/signIn', authController.login);
+userRouter.patch('/', authController.protect, userController.update);
+userRouter.delete('/', authController.protect, userController.delete);

854f4b0574a4a5c2692895a04815641060f33502
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-04-10 16:36:41 -0400

    Update carController.ts

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -2,51 +2,57 @@ import { Request, Response, NextFunction } from 'express';
 import { ICar } from '../interfaces/interfaceCar';
 import CarService from '../services/carService';
 import AppError from '../utils/appErrors';
+import { validateCreateCar } from '../validations/carValidation';
 
 
 class CarController {
     public async findAll(req: Request, res: Response): Promise<void> {
 
-        const events = await CarService.findAll();
+        const cars = await CarService.findAll();
 
         res.status(200).json({
             status: 'success',
-            results: events.length,
+            results: cars.length,
             data: {
-                data: events,
+                data: cars,
             },
         });
     }
 
     public async findById(req: Request, res: Response, next: NextFunction) {
         const { id } = req.params;
-        const event = await CarService.findById(id);
+        const car = await CarService.findById(id);
 
-        if (!event) {
+        if (!car) {
             return next(new AppError('No document found with this id', 404));
         }
 
         res.status(200).json({
             status: 'success',
             data: {
-                data: event,
+                data: car,
             },
         });
     }
 
-    public async create(req: Request, res: Response) {
-        const event = req.body as ICar;
-        const createdEvent = await CarService.create(event);
+    public async create(req: Request, res: Response, next: NextFunction) {
+        const valid = validateCreateCar(req.body);
+        if (valid.error) {
+            next(new AppError('Please provide valid informations', 400));
+        }
+        const car = req.body as ICar;
+        const createdCar = await CarService.create(car);
 
         res.status(201).json({
             status: 'success',
             data: {
-                data: createdEvent,
+                data: createdCar,
             },
         });
     }
 
     public async deleteById(req: Request, res: Response, next: NextFunction) {
+
         const { id } = req.params;
         const doc = await CarService.deleteById(id);
 

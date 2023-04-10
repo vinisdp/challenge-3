8e40c613411589074c1d4e2c51ce0b6b93effd2c
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-04-10 16:29:05 -0400

    Update reserveValidation.ts

--- src/validations/reserveValidation.ts
+++ src/validations/reserveValidation.ts
@@ -5,7 +5,6 @@ export const validateCreateReserve = (reserve: IReserve) => {
     const reserveSchema = Joi.object({
         start_date: Joi.string().required(),
         end_date: Joi.string().required(),
-        id_car: Joi.string().required(),
     });
 
     return reserveSchema.validate(reserve);

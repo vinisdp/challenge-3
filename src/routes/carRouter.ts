e89f0a7fc70228865c636c85735f0b95b1e6122a
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-04-10 14:29:58 -0400

    Update carRouter.ts

--- src/routes/carRouter.ts
+++ src/routes/carRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import authController from '../controllers/authController';
+import carController from '../controllers/carController';
 
 export const carRouter = Router();
 
-carRouter.post('/', authController.protect);
-carRouter.get('/');
-carRouter.get('/:id');
-carRouter.delete('/:id');
+carRouter.post('/', authController.protect, carController.create);
+carRouter.get('/', authController.protect, carController.findAll);
+carRouter.get('/:id', authController.protect, carController.findById);
+carRouter.delete('/:id', authController.protect, carController.deleteById);
 carRouter.patch('/:id', authController.protect);
